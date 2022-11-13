c0cb67e6672511cf67367dea59084c76576a0b53
mmieses01 <manuel.mieses48@gmail.com>
2022-11-13 13:00:43 -0800

    drunks sounds

---
+++ housekeeper.js
@@ -0,0 +1,8 @@
+function housekeeper(years,name,repertory){
+  this.years = years;
+  this.name = name;
+  this.repertory = repertory;
+  this.clean = function(){
+    alert("Cleaning in Process!");
+  }
+}
